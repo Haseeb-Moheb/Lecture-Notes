27046cccd40b018aed31ef594dfcd15375ceb170
Haseebm01 <haseebdr01@gmail.com>
2022-12-03 14:06:33 -0500

    Updating conditional notes & Jeopardy project

---
+++ Unit-2/labs/In class/age_conditional.js
@@ -0,0 +1,26 @@
+/*
+    Look up the format for an Else If statement
+    Set a variable of age and give it a number of your choice
+    Create an else if statement that checks the following:
+    If the age is 17 or younger, console.log 'Sorry, you're too young to do anything.'
+    If the age is at least 18, console.log 'You can vote!'
+    If the age is at least 21, console.log 'You can drink!'
+    If the age is at least 25, console.log 'You can rent a car!'
+*/
+
+let age = 25;
+
+if(age >= 25) {
+    console.log('You can rent a car.');
+} 
+else if(age >= 21) {
+    console.log('You can drink.');
+} 
+else if(age >= 18) {
+    console.log('You can vote.');
+} 
+else if(age <= 17) {
+    console.log(' Sorry, you'r too young to do anything.'); '
+}
+ 
+
